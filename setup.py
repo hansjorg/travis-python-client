e3fd833c11c75ee406863496f89380c3ec5a2f08
Hans Jørgen Hoel <hansjorg@gmail.com>
2014-01-07 02:06:12 +0100

    Use setuptools and require pycrypto

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='Travis CI Python Client',
-      version = '1.0',
-      description = 'Python client for Travis CI',
-      author = 'Hans Jørgen Hoel',
-      author_email = 'hansjorg@gmail.com',
-      url = 'https://github.com/hansjorg/travis-python-client',
-      py_modules = ['travisclient'],
+setup(
+    name='travisclient',
+    version = '1.0',
+    description = 'Python client for Travis CI',
+    author = 'Hans Jørgen Hoel',
+    author_email = 'hansjorg@gmail.com',
+    url = 'https://github.com/hansjorg/travis-python-client',
+    py_modules = ['travisclient'],
+    install_requires = ['pycrypto'],
 )
 
